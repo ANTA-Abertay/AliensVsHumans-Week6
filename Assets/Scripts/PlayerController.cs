aebd2bb251ac419e02dcdedfb35749e5b8a604a5
AileyT <2500455@uad.ac.uk>
2025-10-21 12:42:37 +0100

    Made a start on making the alien face the direction of motion
    it only spins rn

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,12 +10,21 @@ public class PlayerController : MonoBehaviour
     public float speed = 10;
 
     public GameObject bulletPrefab;
-      
+    public Transform target;
+
+    Rigidbody m_Rigidbody;
+    Vector3 m_EulerAngleVelocity;
+
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
         GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
-        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationY;
+        //Fetch the Rigidbody from the GameObject with this script attached
+        m_Rigidbody = GetComponent<Rigidbody>();
+
+        //Set the angular velocity of the Rigidbody (rotating around the Y axis, 100 deg/sec)
+        m_EulerAngleVelocity = new Vector3(0, 100, 0);
+
     }
 
     // This function is called when a move input is detected.
@@ -41,6 +50,8 @@ public class PlayerController : MonoBehaviour
         Rigidbody rb = bullet.GetComponent<Rigidbody>();
         if (rb != null)
             rb.AddForce(new Vector2(_movement.x, _movement.y), ForceMode.Impulse);
+
+       
     }
 
     // FixedUpdate is called once per fixed frame-rate frame.
@@ -51,5 +62,10 @@ public class PlayerController : MonoBehaviour
 
         // Apply force to the Rigidbody to move the player.
         _rb.AddForce(movement * speed);
+
+        Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.fixedDeltaTime);
+        m_Rigidbody.MoveRotation(m_Rigidbody.rotation * deltaRotation);
     }
 }
+
+
