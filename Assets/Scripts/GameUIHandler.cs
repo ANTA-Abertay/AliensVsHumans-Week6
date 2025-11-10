4a42c9c228d41bf0d7f0ec0c67ede87fe6c703e1
AileyT <2500455@uad.ac.uk>
2025-11-10 11:40:29 +0000

    health bar fixed

--- Assets/Scripts/GameUIHandler.cs
+++ Assets/Scripts/GameUIHandler.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -7,100 +6,103 @@ public class GameUIHandler : MonoBehaviour
     public GameManager gameManager;
     public PlayerController player;
     public UIDocument uiDoc;
+
     private VisualElement _mHealthBarMask;
     private Label _mHealthLabel;
     private Label _mLevelLabel;
+
     private readonly int _maxHealth = 10;
     private int _health;
     private int _currentHealth = 10;
     private int _currentLevel;
-    private int _level = 1;
+    private int _level;
+
     private float _timer;
+    private bool _labelVisible = false;
+
     private void Start()
     {
         _mHealthBarMask = uiDoc.rootVisualElement.Q<VisualElement>("HealthBarMask");
         _mHealthLabel = uiDoc.rootVisualElement.Q<Label>("HealthLabel");
         _mLevelLabel = uiDoc.rootVisualElement.Q<Label>("LevelLabel");
+        
+        _mLevelLabel.style.display = DisplayStyle.None;
+        _labelVisible = false;
         HealthChanged();
+        _level = gameManager.currentLevel;
     }
 
     private void Update()
     {
+       _timer -= Time.deltaTime; 
+        if (_labelVisible)
+        {
+            if (_timer <= 0)
+            {
+                HideLevelLabel();
+            }
+        }
         _currentLevel = gameManager.currentLevel;
         _health = player.health;
-        _timer -= Time.deltaTime;
-        if(_currentHealth != _health)
+        
+        if (_currentHealth != _health)
         {
             HealthChanged();
             _currentHealth = _health;
-            
         }
-
         
-        if (_level != _currentLevel);
+        if (_level != _currentLevel)
         {
-            levelChange();
             _level = _currentLevel;
+            LevelChange();
         }
-
-        if (_health == 0);
+        
+        if (_health <= 0)
         {
             Death();
         }
-        if (_currentLevel == 20);
+        
+        if (_currentLevel == 20)
         {
             Win();
         }
-
     }
-
     void HealthChanged()
     {
-        _mHealthLabel.text = $"{player.health}/{_maxHealth}";
-        float healthPercent = 100f * ((float)player.health / _maxHealth);
+        _mHealthLabel.text = $"{_currentHealth}/{_maxHealth}";
+        float healthPercent = ( (float)_currentHealth / (float)_maxHealth ) * 100f;
         _mHealthBarMask.style.width = new Length(healthPercent, LengthUnit.Percent);
-
+        _mHealthBarMask.MarkDirtyRepaint();
     }
-
-    void levelChange()
+    void LevelChange()
     {
-        while(_timer >= -4)
-        {
-            _timer = 0;
-            _mLevelLabel.text = $"{"level Complete"}";
-            if (_timer == -2)
-            {
-                _mLevelLabel.text = $"{"level"}currentLevel";
-            }
-            
-            
-        }
-        
-        
+        _timer = 2f; // visible for 2 seconds
+        ShowLevelLabel("Level Complete!");
     }
-    
+
     void Win()
     {
-        while (_timer >= -4)
-        {
-            _timer = 0;
-            _mLevelLabel.text = $"{"You Win"}";
-            if (_timer == -2)
-            {
-                _mLevelLabel.text = $"{"INFINITE MODE"}";
-            }
+        _timer = 3f; // visible for 3 seconds
+        ShowLevelLabel("You Win!");
+    }
 
-        }
-    }
-    
     void Death()
-    { 
-        _timer = 2;
-        while (_timer >= 0)
-        {
-            _mLevelLabel.text = $"{"GAME OVER"}";
-        }
-        
+    {
+        _timer = 3f;
+        ShowLevelLabel("GAME OVER");
     }
     
+    void ShowLevelLabel(string message)
+    {
+        _mLevelLabel.text = message;
+        _mLevelLabel.style.display = DisplayStyle.Flex;
+        _labelVisible = true;
+    }
+
+    void HideLevelLabel()
+    {
+        _mLevelLabel.style.display = DisplayStyle.None;
+        _labelVisible = false;
+        _timer = 0;
+    }
 }
